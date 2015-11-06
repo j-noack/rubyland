b5d0cd09d3222658005a643cdac09b0f4d9a17de
Lydethlas <a.f.m.broeker@web.de>
2015-11-06 12:39:55 +0100

    5x end eingefügt

--- src/CollisionManager.rb
+++ src/CollisionManager.rb
@@ -2,61 +2,67 @@
 class CollisionManager
 
 	attr_accessor :map
-	
+
 	def initialize(map)
 		@map = map
 	end
-	
+
     def update
 		# player => @map.player
 		# enemies => @map.enemies
-		
-		
+
+
     end
-	
+
 	def checkCircleCollision(obj1, obj2)
 		if (getDistance(obj1, obj2) < (obj1.width / 2) + (obj2.width / 2))
 			return true
 		else
 			return false
+		end
 	end
-	
+
 	def getDistance(obj1, obj2)
-		return Math.sqrt(((obj1.x - obj2.x) ** 2) + ((obj1.y - obj2.y) ** 2)) 
+		return Math.sqrt(((obj1.x - obj2.x) ** 2) + ((obj1.y - obj2.y) ** 2))
 	end
-	
+
 	def mapBorderCollision(map, obj)
 		if (checkLeft(obj) || checkRight(obj, map) || checkUp(obj) || checkDown(obj, map))
 			return true
-		else 
+		else
 			return false
+		end
 	end
-	
+
 	def checkLeft(obj)
 		if (obj.x - obj.width < 0)
 			return true
 		else
 			return false
+		end
 	end
-	
+
 	def checkRight(obj, map)
 		if (obj.x + obj.width > map.width)
 			return true
 		else
 			return false
+		end
 	end
-	
+
 	def checkUp(obj)
 		if (obj.y - obj.height < 0)
 			return true
 		else
 			return false
+		end
 	end
-	
+
 	def checkDown(obj, map)
 		if (obj.y + obj.height > map.height)
 			return true
 		else
 			return false
+		end
 	end
 end
