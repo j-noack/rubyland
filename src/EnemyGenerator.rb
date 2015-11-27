1ccd43f5f5d951c12e0389906152b6265c1f59d8
Lydethlas <a.f.m.broeker@web.de>
2015-11-27 11:03:45 +0100

    Enemy Spawn behaviour changed (no more stuck enemies & instadeaths)

--- src/EnemyGenerator.rb
+++ src/EnemyGenerator.rb
@@ -12,32 +12,34 @@ class EnemyGenerator
     return wave1
   end
 
-  def wave0
-    enemy = Enemy.new
-    enemy.x = 50
-    enemy.y = 50
-    enemy.target = @target
-    enemy.speed = 1
-
-    enemy2 = Enemy.new
-    enemy2.x = 800
-    enemy2.y = 500
-    enemy2.target = @target
-    enemy2.speed = 1.5
-
-    return [enemy, enemy2]
-  end
-
   def wave1
     enemies = []
 
     mapW = @map.width - (@map.borderWidth * 2)
     mapH = @map.height - (@map.borderWidth * 2)
+    puffer = 4
 
     50.times do |i|
       enemy = Enemy.new
-      enemy.x = enemy.width + @map.borderWidth + rand(mapW - enemy.width * 2)
-      enemy.y = enemy.width + @map.borderWidth + rand(mapH - enemy.width * 2)
+      spriteSize = (0.5 * (enemy.width+1))+puffer
+      area = rand(4)
+      if (area==0)
+        enemy.x = @map.borderWidth + spriteSize + rand(mapW - (spriteSize * 2))
+        enemy.y = @map.borderWidth + spriteSize + rand(50)
+      end
+      if (area==1)
+        enemy.x = @map.borderWidth + spriteSize + rand(50)
+        enemy.y = @map.borderWidth + spriteSize + rand(mapH - (spriteSize * 2))
+      end
+      if (area==2)
+        enemy.x = @map.borderWidth + spriteSize + rand(mapW - (spriteSize * 2))
+        enemy.y = @map.borderWidth - spriteSize + mapH - 50 + rand(50)
+      end
+      if (area==3)
+        enemy.x = @map.borderWidth - spriteSize + mapW - 50 + rand(50)
+        enemy.y = @map.borderWidth + spriteSize + rand(mapH - (spriteSize * 2))
+      end
+
       enemy.target = @target
       enemy.speed = rand(150) * 0.01 + 0.1
 
